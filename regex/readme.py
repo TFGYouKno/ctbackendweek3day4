9b27d3f609b3e3a3e461bc30acf1c09d15a15e15
TFGYouKno <brockhoffharrison@comcast.net>
2024-07-11 23:34:57 -0400

    initial commit

---
+++ regex/readme.py
@@ -0,0 +1,22 @@
+#Problem Statement:  Write a function that takes in a list of names, and verifies that the names are valid names using a regex pattern and match(), and prints the name if it is valid, "Invalid name" if the name is not.
+
+#Use the following list as an argument to test your function.
+
+
+
+#Code Example:
+
+
+
+#names = ["Abraham Lincoln", "Andrew P Garfield", "peter pan", "Connor Milliken", "Jordan Alexander Williams", "Madonna", "programming is cool"]
+
+
+#Expected Outcome:
+
+#Abraham Lincoln
+#Andrew P Garfield
+#Invalid name
+#Connor Milliken
+#Jordan Alexander Williams
+#Invalid name
+#Invalid name
